cfede6bf439315eb43fa8485bb2810ecdb04b9a2
Aryan Setia <aryansetiaa@gmail.com>
2024-01-20 23:33:38 +0530

    added templates

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '3e89a5fec09ceabb770571cac2a32d10'
+
+    from .views import views 
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
     return app
