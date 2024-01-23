3ad1cdee2df8410da2bc65b2f6d2ec4b9ab30660
Aryan Setia <aryansetiaa@gmail.com>
2024-01-23 23:53:44 +0530

    added notes model with foreign key relationship

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path 
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -7,7 +8,8 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '3e89a5fec09ceabb770571cac2a32d10'
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{app.root_path}/site.db"
+
 
     db.init_app(app)
 
@@ -16,5 +18,17 @@ def create_app():
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+    
+    from .models import User, Note 
+    create_database(app)
 
     return app
+
+
+def create_database(app): 
+    if not path.exists('website/' + DB_NAME): 
+        # Create the database (if it doesn't exist)
+        with app.app_context():
+            db.create_all()        
+        print('Database Created!')
+    
--- website/models.py
+++ website/models.py
@@ -1,8 +1,27 @@
 from . import db  
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from flask_login import UserMixin
+from datetime import datetime 
+import pytz 
+from typing import List
+
+
+IST = pytz.timezone("Asia/Kolkata")
+
+
 
 class User(db.Model, UserMixin): 
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.string(150))
-    first_name = db.Column(db.string(150))
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] =  mapped_column(String(150))
+    password: Mapped[str] = mapped_column(String(150))
+    first_name: Mapped[str] = mapped_column(String(150))
+    children: Mapped[List["Note"]] = relationship() 
+
+# one to many - that means one user can create multiple notes 
+class Note(db.Model): 
+    id: Mapped[int] = mapped_column(primary_key=True)
+    data: Mapped[str] = mapped_column(String(10000))
+    created_at: Mapped[datetime] = mapped_column(default=lambda: datetime.now(IST))
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id")) # note attached to a specific user.id   
